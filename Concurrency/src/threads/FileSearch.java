44a85f98f7fc290d6623959a91e46d173e73ce8b
pardhu-pokuri <pardhu.sona@gmail.com>
2016-11-24 22:59:36 +0530

    Filesearch thread
    Filesearch thread

---
+++ Concurrency/src/threads/FileSearch.java
@@ -0,0 +1,65 @@
+package threads;
+
+import java.io.File;
+
+public class FileSearch implements Runnable{
+
+	private String path;
+	private String fileName;
+	
+	public FileSearch(String path, String fileName) {
+		this.path = path;
+		this.fileName = fileName;
+	}
+	
+	
+	@Override
+	public void run() {
+		
+		File file = new File(path);
+		try{
+			if(file.isDirectory()){
+				processDirectory(file);
+			}else{
+				processFile(file);
+			}
+		}catch(InterruptedException e){
+			System.out.println("Directory Search aborted");
+		}
+	}
+
+
+	private void processFile(File file) throws InterruptedException {
+		
+		if(file.getName().contains(fileName)){
+			System.out.println("File found at : " + file.getAbsolutePath());
+		}
+		
+		if(Thread.currentThread().isInterrupted()){
+			throw new InterruptedException();
+		}
+	}
+
+
+	private void processDirectory(File file) throws InterruptedException {
+		
+		File[] files = file.listFiles();
+		
+		for(File eachFile : files){
+			
+			if(eachFile.isDirectory()){
+				processDirectory(eachFile);
+			}else{
+				processFile(eachFile);
+			}
+		}
+		
+		if(Thread.currentThread().isInterrupted()){
+			throw new InterruptedException();
+		}
+		
+	}
+
+
+
+}
---
+++ Concurrency/src/threads/FileSearchThreadInterruption.java
@@ -0,0 +1,25 @@
+package threads;
+
+import java.util.concurrent.TimeUnit;
+
+public class FileSearchThreadInterruption {
+
+	public static void main(String[] args) {
+		
+		FileSearch searchRunnable = new FileSearch("D:\\", "720p");
+		Thread thread = new Thread(searchRunnable);
+		thread.start();
+		
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		thread.interrupt();
+		
+	}
+	
+	
+	
+}
