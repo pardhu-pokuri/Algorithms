33b31b528b86ee5179b6c29d31e6938b71cf5eef
pardhu-pokuri <pardhu.sona@gmail.com>
2017-02-15 23:16:46 +0530

    adding account class

---
+++ Concurrency/src/ch2_basicthreadsync/Account.java
@@ -0,0 +1,51 @@
+package ch2_basicthreadsync;
+
+
+/**
+ * model for account
+ * 
+ *
+ */
+public class Account {
+	
+	private double balance;
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	/**
+	 * increase balance by given amount 
+	 * @param amount
+	 */
+	public void addAmount(double amount){
+		double tmp = balance;
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		tmp = tmp + amount;
+		balance = tmp;
+	}
+	
+	/**
+	 * decreases balance by given amount
+	 * @param amount
+	 */
+	public void subtractAmount(double amount){
+		double tmp = balance;
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		tmp = tmp - amount;
+		balance = tmp;
+	}
+	
+}
